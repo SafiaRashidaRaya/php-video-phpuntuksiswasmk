1255d9f4461aa88ee894c6318428de1b414d2267
SafiaRashidaRaya <140325055+SafiaRashidaRaya@users.noreply.github.com>
2024-01-09 08:40:39 +0700

    versi 1.0
     Menampilkan Tulisan dan HTML

---
+++ lat01.php
@@ -0,0 +1,11 @@
+<?php 
+    echo "belajar";
+    echo "<br>";
+    echo "siswa";
+    echo "<br>";
+    echo "<h1 style='background-color:red'>Hai</h1>
+        <p style='background-color:blue'>Saya ngantuk sekali</p>
+    ";
+    echo "Saya " ." belajar PHP." ."<br>";
+    echo "Year ". 2019 + 5;
+?>
