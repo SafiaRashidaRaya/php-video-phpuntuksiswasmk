40a475b2c988898c0ee31dc54c275147501a38cb
SafiaRashidaRaya <140325055+SafiaRashidaRaya@users.noreply.github.com>
2024-01-09 08:51:35 +0700

    versi 1.1
    Variabel Tipe Data dan Komentar

---
+++ lat02.php
@@ -0,0 +1,13 @@
+<?php 
+
+    $tulisan = "I'm stu-dying.";
+    echo $tulisan .'<br>';
+
+    $tahun = 20.24;
+    echo "Tahun ". (int)$tahun. "<br>";
+
+    var_dump($tulisan);
+    echo "<br>";
+    var_dump($tahun);
+
+?>
