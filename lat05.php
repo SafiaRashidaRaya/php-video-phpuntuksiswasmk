a77f2aaa790e0a91d45035f337c782a63ab8dc62
SafiaRashidaRaya <140325055+SafiaRashidaRaya@users.noreply.github.com>
2024-01-11 13:29:19 +0700

    versi 1.4
    Pengujian IF

---
+++ lat05.php
@@ -0,0 +1,26 @@
+<?php 
+
+    $tanggal = 31;
+
+    if ($tanggal < 32) {
+        if ($tanggal > 0 ) {
+            echo 'benar';
+        } else {
+            echo 'salah';
+        }
+        
+    } else {
+        echo 'salah';
+    }
+
+    echo '<br>';
+    
+    $nilai = 110;
+
+    if ($nilai <= 100 && $nilai >= 0) {
+        echo 'benar';
+    } else {
+        echo 'salah';
+    }
+    
+?>
