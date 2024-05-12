8ad4c7991ecc5627cc1558092dd326382544fe12
SafiaRashidaRaya <140325055+SafiaRashidaRaya@users.noreply.github.com>
2024-05-12 20:57:21 +0700

    Create array.php
    versi 1.7

---
+++ array.php
@@ -0,0 +1,43 @@
+<?php 
+// $nama = array('satu', 'dua', 'tiga', 4);
+// var_dump($nama);
+// echo '<br>';
+// echo $nama[3];
+
+// // for ($i = 0; $i < 4; $i++) {
+// //     // echo $i;
+// //     echo $nama[$i].'<br>';
+// // };
+
+// foreach ($nama as $k ){
+//     echo $k.'<br>';
+// }
+
+
+// $nama = array(
+// "joni" => "surabaya",
+// "budi"=> "malang",
+// "tejo" => "jakarta",
+// "siti" => "sidoarjo",
+// );
+
+$nama['joni'] = 'surabaya';
+$nama['budi'] = 'malang';
+$nama['tejo'] = 'jakarta';
+$nama['siti'] = 'sidoarjo';
+$nama['kelima'] = 'irian jaya';
+
+var_dump($nama);
+
+echo"<br>";
+
+// echo $nama['siti'];
+
+foreach($nama as $key => $value) {
+echo $key. "=>". $value;
+echo"<br>";
+
+}
+
+?>
+
