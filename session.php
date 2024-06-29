0be294150df5298b4ec0a9a233fcb83c1d347a13
SafiaRashidaRaya <140325055+SafiaRashidaRaya@users.noreply.github.com>
2024-06-29 22:00:18 +0700

    versi 2.1

---
+++ session.php
@@ -0,0 +1,48 @@
+<nav>
+    <ul>
+        <li><a href="?menu=isi">isi</a></li>
+        <li><a href="?menu=hapus">hapus</a></li>
+        <li><a href="?menu=destroy">destroy</a></li>
+    </ul>
+</nav>
+
+
+<?php
+
+session_start();
+
+if (isset($_GET['menu'])) {
+    $menu = $_GET['menu'];
+    
+    echo $menu;
+
+    switch ($menu) {
+        case 'isi':
+            isiSession();
+            break;
+        case 'hapus':
+            unset($_SESSION['user']);
+            break;
+        case 'destroy':
+            session_destroy();
+            break;
+        default:
+            # code...
+            break;
+    }
+}
+
+echo '<br>';
+
+var_dump($_SESSION);
+
+function isiSession()
+{
+    $_SESSION['user'] = 'joni';
+
+    $_SESSION['nama'] = 'nama orang';
+
+    $_SESSION['alamat'] = 'sidoarjo';
+}
+
+?>
