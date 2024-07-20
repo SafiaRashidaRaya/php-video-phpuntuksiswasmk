bd6cde335a5bb26261d11cccefb3362c48757a27
SafiaRashidaRaya <140325055+SafiaRashidaRaya@users.noreply.github.com>
2024-07-20 22:59:57 +0700

    versi 2.9

---
+++ restoran/db.php
@@ -0,0 +1,47 @@
+<?php 
+    class DB{
+        public $host = "127.0.0.1";
+        private $user = "root";
+        private $password = "";
+        private $database = "dbrestoran";
+    
+        public function __construct(){
+            echo "construct";
+        }
+
+        public function selectData(){
+            echo 'select data';
+        }
+
+        public function getDatabase(){
+            return $this->database;
+        }
+
+        public function tampil(){
+            $this->selectData();
+        }
+
+        public static function insertData(){
+            echo "static function";
+        }
+    }
+
+    DB::insertData();
+
+    // $db = new DB;
+
+    // echo '<br>';
+
+    // $db->selectData();
+
+    // echo '<br>';
+    
+    // echo $db->host;
+
+    // echo '<br>';
+
+    // echo $db->getDatabase(). '<br>';
+
+    // $db->tampil();
+
+?>
